b0eaac8c77eeeb15af4900472de154e228c4f695
debo9210 <omololuadebowale1@gmail.com>
2022-06-20 15:54:53 +0800

    ICO contract folder

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,5 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS =
+  '0xaE2D4b59f5d971E9aC4971368bba878d5A784D1E';
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
